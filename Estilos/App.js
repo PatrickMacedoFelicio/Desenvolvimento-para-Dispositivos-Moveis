323dc2abd8c4c944618f1911954ca4aa7bc72521
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-05-16 21:53:20 -0400

    up

--- Estilos/App.js
+++ Estilos/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StyledButton from './components/StyledButton';
+import ProfileCard from './components/ProfileCard';
+import DraculaCard from './components/DraculaCard';
 
 export default function App() {
 
@@ -9,11 +11,21 @@ export default function App() {
   return (
     <View style={styles.container}>
 
+      <DraculaCard
+        title={"Titulo teste"}
+        content={"Conteudo nada importante"}
+      />
+
+      {/* <ProfileCard
+        nome='Jack Twist'
+        avatarUri='https://carboncostume.com/wordpress/wp-content/uploads/2020/04/jack-twist-from-brokeback-mountain.jpg'
+        bio='Real Cowboy ride C'
+      />
 
       <StyledButton
         title='Clique aqui'
         onPress={handlePress}
-      />
+      /> */}
 
 
       <StatusBar style="auto" />
